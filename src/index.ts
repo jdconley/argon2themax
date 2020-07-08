a031fb1a53e09cde5fc8e2799ceebb274936290c
Vinh <vinh@brava.com>
2020-07-07 17:20:41 -0700

    add reference url

--- src/index.ts
+++ src/index.ts
@@ -39,6 +39,8 @@ export const hash:
     (plain: Buffer | string, salt: Buffer, options?: Options) => Promise<string>
     = argon2lib.hash;
 
+// This used to be defined in argon2lib.generateSalt but then being remove
+// https://github.com/ranisalt/node-argon2/commit/72fed64dc752a97613a0a63143b810b35ee69abf#diff-04c6e90faac2675aa89e2176d2eec7d8L24
 export async function generateSalt(length?: number): Promise<Buffer> {
     return new Promise((resolve, reject) => {
         crypto.randomBytes(length || 16, (err, salt) => {
